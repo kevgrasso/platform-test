fc34cfdf1b5b7d39e549aec626b22bf78e6740ab
Kevyn Grasso <kevgrasso@gmail.com>
2024-10-01 18:08:28 -0400

    adjust camera offset to lock aperature to the correct cell

--- src/MainCamera.cs
+++ src/MainCamera.cs
@@ -9,7 +9,10 @@ public partial class MainCamera : Camera2D {
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public void PickActiveScreen(Vector2 pos) {
-		Rect2 aperture = _hud.GetApertureRect();
+		Rect2 aperture = _hud.GetApertureRect(); // the "window" of the gui cockpit
+		// position camera to correct cell
 		Position = (pos/aperture.Size).Floor() * aperture.Size;
+		//adjust camera for os window aspect ration
+		Offset = -aperture.Position;
 	}
 }

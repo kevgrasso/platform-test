414ccc882d49bdfacaeaa8448444016ec4f7c184
Kevyn Grasso <kevgrasso@gmail.com>
2024-10-21 20:26:39 -0400

    update camera and move player script

---
+++ lib/resources/InfoManager.cs
@@ -0,0 +1,36 @@
+using Godot;
+
+[GlobalClass] public partial class InfoManager : Node {
+	static private Player _player;
+	static private ReferenceRect _aperture;
+
+	static public void RegisterPlayer(Player player) {
+		_player = player;
+	}
+
+	static public void RegisterAperture(ReferenceRect aperture) {
+		_aperture = aperture;
+	}
+
+	static public Vector2I GetPlayerCell(Vector2 grid_offset, Vector2 cell_size) {
+		return (Vector2I)((grid_offset+_player.GlobalPosition)/cell_size).Floor();
+	}
+
+	static public Vector2I GetPlayerBoard() {
+		return GetPlayerCell(Vector2.Zero, _aperture.Size);
+	}
+
+	static public Vector2 GetPlayerBoardPos() {
+		return GetPlayerBoard() * _aperture.Size;
+	}
+
+	static public Rect2 GetApertureRect() {
+		return new Rect2(_aperture.GlobalPosition, _aperture.Size);
+	}
+
+	static public float GetInputDirection() {
+		return Mathf.Sign(
+			Input.GetActionStrength("move_right") - Input.GetActionStrength("move_left")
+		);
+	}
+}
--- src/hud/MainCamera.cs
+++ src/hud/MainCamera.cs
@@ -1,20 +1,18 @@
 using Godot;
 
 public partial class MainCamera : Camera2D {
-	private CockpitHUD _hud;
 
 	public override void _Ready() {
 		int min_w = (int)ProjectSettings.GetSetting("display/window/size/viewport_width");
 		int min_h = (int)ProjectSettings.GetSetting("display/window/size/viewport_height");
 		GetWindow().MinSize = new Vector2I(min_w, min_h);
-		_hud = GetNode<CockpitHUD>("/root/Game/GUILayer/CockpitHUD");
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public void PickActiveScreen(Vector2 pos) {
-		Rect2 aperture = _hud.GetApertureRect(); // the "window" of the gui cockpit
+	public void UpdateActiveBoard(Vector2 pos) {
+		Rect2 aperture = InfoManager.GetApertureRect(); // the "window" of the gui cockpit
 		// position camera to correct cell
-		Position = (pos/aperture.Size).Floor() * aperture.Size;
+		Position = InfoManager.GetPlayerBoardPos();
 		//adjust camera for os window aspect ration
 		Offset = -aperture.Position;
 	}
